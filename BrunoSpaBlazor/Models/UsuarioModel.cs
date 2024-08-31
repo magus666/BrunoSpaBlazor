47fac2c94b04897cddcffeb9bbaa5294da6d6bef
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2024-08-31 12:42:16 -0500

    Modificaciones en la configuracion de la tabla

--- BrunoSpaBlazor/Models/UsuarioModel.cs
+++ BrunoSpaBlazor/Models/UsuarioModel.cs
@@ -7,10 +7,20 @@ namespace BrunoSpaBlazor.Models
         public int IdUsuario { get; set; } = 1;
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras.")]
         public string NombreUsuario { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras.")]
         public string ApellidoUsuario { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [RegularExpression(@"^[0-9\s]+$", ErrorMessage = "Solo se permiten Numero.")]
+        public string DocumentoUsuario { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Por favor, introduce un correo electrónico válido.")]
+        public string CorreoElectronicoUsuario { get; set; } = string.Empty;
 
         [Range(1, 100, ErrorMessage = "La edad debe ser mayor que 0 y menor o igual a 100.")]
         public int EdadUsuario { get; set; }
